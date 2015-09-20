5f0767a87ab7d34ceaf7b4f97f4b8ca45241bee0
Xiaolong Lin <Xiaolong.Lin@blackboard.com>
2015-09-20 14:02:58 +0800

    init node server, finish a simple web app

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cftvc
-cftvc
+cftvc 1.0.0
+
+How to Set Up:
+
+1. git clone https://github.com/one-pieces/cftvc.git
+2. cd cftvc
+3. npm install
+4. bower install
+5. node app.js
+6. 浏览器输入localhost:5800
---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var ejs = require('ejs');
+
+var app = express();
+var server = require('http').createServer(app);
+
+app.set('port', process.env.PORT || 5800);
+app.set('views', __dirname + '/app');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html'); //替换文件扩展名ejs为html
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+server.listen(app.get('port'), function() {
+	console.log('cftvc is listening port ' + app.get('port'));
+});
