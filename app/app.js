ce1b6936085e6b8520102b8d661f0d52494554cc
Xiaolong Lin <Xiaolong.Lin@blackboard.com>
2015-10-18 02:01:38 +0800

    update

--- app/app.js
+++ app/app.js
@@ -12,6 +12,16 @@ app.config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
 				url: '/cftvc/home',
 				templateUrl: 'views/features/home.html',
 				controller: 'homeCtrl'
+			})
+			.state('detail', {
+				url: '/cftvc/detail',
+				templateUrl: 'views/features/detail.html',
+				controller: 'detailCtrl'
+			})
+			.state('announcement', {
+				url: '/cftvc/announcement',
+				templateUrl: 'views/features/announcement.html',
+				controller: 'announcementCtrl'
 			});
 		$locationProvider.html5Mode(true);
 	}]);
